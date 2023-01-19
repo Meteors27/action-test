9bd912e9dd478846e26daba6753b21c030a8780d
Meteors27 <williamxia27@qq.com>
2023-01-19 20:03:45 +0800

    update

--- script.py
+++ script.py
@@ -1,1 +1,7 @@
-print("hello, world. I am a script. ")
+import sys
+import requests as re
+# token = sys.argv[1]
+# url = "https://api.day.app/" + token + "/Hi,William."
+# res = re.get(url=url)
+# print(res.status_code)
+print("hello")
