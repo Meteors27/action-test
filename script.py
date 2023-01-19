45700bbba0a3815daa489ce65781530bceb511df
Meteors27 <williamxia27@qq.com>
2023-01-19 22:35:38 +0800

    update

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import sys
 import requests as re
 # token = sys.argv[1]
-# url = "https://api.day.app/" + token + "/Hi,William."
+# url = "https://api.day.app/" + token + "/Hello"
 # res = re.get(url=url)
 # print(res.status_code)
 print("hello")
