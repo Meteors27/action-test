ddb399d54d5ace383d4cf114ad7cf06dfccc83d0
Meteors27 <williamxia27@qq.com>
2023-01-19 23:38:05 +0800

    update

--- script.py
+++ script.py
@@ -1,19 +1,15 @@
-import argparse
 import os
 import requests as re
-# parser = argparse.ArgumentParser(description='Get Bark ID')
-# parser.add_argument('--user', '-u', help='Bark ID', required=True)
-# args = parser.parse_args()
+import argparse
 
 if __name__ == '__main__':
-    # try:
-    #     print("args.user = "+args.user)
-    #     url = "https://api.day.app/" + args.user + "/Hello"
-    #     res = re.get(url=url)
-    #     print(res.status_code)
-    # except Exception as e:
-    #     print(e)
-    user = os.environ['user']
-    url = "https://api.day.app/" + user + "/Hello"
-    res = re.get(url=url)
-    print(res.status_code)
+    try:
+        user = os.environ['user']
+        title = "竺可桢学院"
+        content = "关于2022-2023年竺可桢奖学金评选结果的通知"
+        link = "https://www.baidu.com"
+        url = f"https://api.day.app/{title}/{content}/?url={link}"
+        res = re.get(url=url)
+        # print(res.status_code)
+    except Exception as e:
+        print(e)
