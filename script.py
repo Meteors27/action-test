056539b76f659e83a8fe6dee3e1ad607007e951a
Meteors27 <williamxia27@qq.com>
2023-01-19 23:24:02 +0800

    update

--- script.py
+++ script.py
@@ -1,14 +1,19 @@
 import argparse
+import os
 import requests as re
-parser = argparse.ArgumentParser(description='Get Bark ID')
-parser.add_argument('--user', '-u', help='Bark ID', required=True)
-args = parser.parse_args()
+# parser = argparse.ArgumentParser(description='Get Bark ID')
+# parser.add_argument('--user', '-u', help='Bark ID', required=True)
+# args = parser.parse_args()
 
 if __name__ == '__main__':
-    try:
-        print("args.user = "+args.user)
-        url = "https://api.day.app/" + args.user + "/Hello"
-        res = re.get(url=url)
-        print(res.status_code)
-    except Exception as e:
-        print(e)
+    # try:
+    #     print("args.user = "+args.user)
+    #     url = "https://api.day.app/" + args.user + "/Hello"
+    #     res = re.get(url=url)
+    #     print(res.status_code)
+    # except Exception as e:
+    #     print(e)
+    userID = os.environ['userID']
+    url = "https://api.day.app/" + userID + "/Hello"
+    res = re.get(url=url)
+    print(res.status_code)
