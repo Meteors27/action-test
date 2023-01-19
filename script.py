048322a75f426002a2d64457dc859406da9f7db5
Meteors27 <williamxia27@qq.com>
2023-01-19 23:58:56 +0800

    last commit before my sound sleep

--- script.py
+++ script.py
@@ -8,7 +8,8 @@ if __name__ == '__main__':
         title = "竺可桢学院"
         content = "关于2022-2023年竺可桢奖学金评选结果的通知"
         link = "https://www.baidu.com"
-        url = f"https://api.day.app/{user}/{title}/{content}?url={link}?icon=https://raw.github.com/Meteors27/action-test/icon/ckcLogo.png"
+        icon = "https://raw.github.com/Meteors27/action-test/icon/ckcLogo.png"
+        url = f"https://api.day.app/{user}/{title}/{content}?url={link}?icon={icon}"
         re.get(url=url)
     except Exception as e:
         print(e)
